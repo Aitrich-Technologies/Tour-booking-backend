e4f8487dd566638840337fc9d06f669742e405b8
AFRA-AFI <afra.afi.74@gmail.com>
2025-09-13 10:11:10 +0530

    added

--- TourBooking/Domain/Models/Notes.cs
+++ TourBooking/Domain/Models/Notes.cs
@@ -1,22 +1,12 @@
 ﻿using Domain.Enum;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Domain.Models
+public partial class Notes
 {
-    public partial class Notes
-    {
-        public Guid Id { get; set; }
+    public Guid Id { get; set; }
+    public Guid TourId { get; set; }
 
-        public Guid TourId { get; set; }
+    public string? TourNotes { get; set; }
+    public NotesStatus Status { get; set; }
 
-        public string? TourNotes { get; set; }
-
-        public NotesStatus Status { get; set; }
-
-        public virtual Tour Tour { get; set; } = null!;
-    }
+    public virtual Tour Tour { get; set; } = null!;
 }
--- TourBooking/Domain/Models/Tour.cs
+++ TourBooking/Domain/Models/Tour.cs
@@ -1,39 +1,31 @@
 ﻿using Domain.Enum;
-using System;
-using System.Collections.Generic;
-
-namespace Domain.Models;
+using Domain.Models;
 
 public partial class Tour
 {
     public Guid Id { get; set; }
 
     public string TourName { get; set; } = null!;
-
     public string? TourDescription { get; set; }
 
     public Guid DestinationId { get; set; }
-
     public int? NoOfNights { get; set; }
-
     public int Price { get; set; }
-
     public DateOnly? DepartureDate { get; set; }
-
     public DateOnly? ArrivalDate { get; set; }
 
-    public AuthUser Customer { get; set; }
+    // Foreign keys for AuthUser
+    public Guid CustomerId { get; set; }
+    public Guid ConsultantId { get; set; }
 
     public TourStatus Status { get; set; }
 
-    public AuthUser Consultant { get; set; }
-
-    public Guid TermsId { get; set; }
-
+    // Navigation properties
     public virtual Destination Destination { get; set; } = null!;
+    public virtual AuthUser Customer { get; set; } = null!;
+    public virtual AuthUser Consultant { get; set; } = null!;
 
     public virtual ICollection<TermsAndCondition> TermsAndConditions { get; set; } = new List<TermsAndCondition>();
-    public virtual ICollection<TourBookingForm> Notes { get; set; } = new List<TourBookingForm>();
-
+    public virtual ICollection<Notes> Notes { get; set; } = new List<Notes>();
     public virtual ICollection<TourBookingForm> TourBookingForms { get; set; } = new List<TourBookingForm>();
 }
