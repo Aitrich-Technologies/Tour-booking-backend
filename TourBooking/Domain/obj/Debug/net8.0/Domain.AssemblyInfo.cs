fa99462314ab4e60d15b2a36306c10237442a351
AFRA-AFI <afra.afi.74@gmail.com>
2025-10-07 16:43:46 +0530

    added

--- TourBooking/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ TourBooking/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeaa12e14e28b3d25bd6b188cf5df09605e0c8fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ec40e9b230a9e4a6189e7a98bf89b71dd2e4d29")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
